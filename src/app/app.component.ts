1666fef2053d2023f2fe4c2be5a91f96c36bf436
Joseph Chan <lonqer@gmail.com>
2018-11-09 10:00:36 +0800

    Use active cell if offset one is out of range

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,34 +14,53 @@ export class AppComponent implements OnInit {
     private http: HttpClient
   ) {}
 
-  welcomeMessage = 'Welcome';
   refs$: Observable<any[]>;
   currtxt: string;
 
   async ngOnInit() {
     try {
       await Excel.run(async context => {
-        context.workbook.onSelectionChanged.add((window as any).Zone.current.wrap(async (args: Excel.SelectionChangedEventArgs) => {
-          let activeCell = args.workbook.getActiveCell();
-          let srctxtCell = activeCell.getOffsetRange(0, -1);
-          try {
-            srctxtCell.load('text');
-            await context.sync();
-          } catch (outOfRangeErr) { }
-          
-          this.currtxt = srctxtCell.text[0][0];
-
-          if (!this.currtxt)
-            return;
-
-          this.refs$ = this.http.get<{ r: any[] }>('/api/search', { params: { q: this.currtxt } })
-            .pipe(map(res => res.r));
-        }, 'onSelectionChangedCallback'));
+        context.workbook.onSelectionChanged.add(this.selectionChangeHandlerFn());
         await context.sync();
       });
     } catch (error) {
       OfficeHelpers.UI.notify(error);
       OfficeHelpers.Utilities.log(error);
     }
+  }
+
+  private selectionChangeHandlerFn(): (args: Excel.SelectionChangedEventArgs) => Promise<any> {
+    return (window as any).Zone.current.wrap(this.onSelectionChanged.bind(this), 'onSelectionChangedCallback');
+  }
+
+  async onSelectionChanged(args: Excel.SelectionChangedEventArgs) {
+    try {
+      await this._onSelectionChanged(args);
+    } catch (err) {
+      OfficeHelpers.UI.notify(err);
+      OfficeHelpers.Utilities.log(err);
+    }
+  }
+
+  private async _onSelectionChanged(_: Excel.SelectionChangedEventArgs) {
+    await Excel.run(async ctx => {
+      let activeCell = ctx.workbook.getActiveCell();
+      let srctxtCell = activeCell.getOffsetRange(0, -1);
+      try {
+        srctxtCell.load('text');
+        await ctx.sync();
+        this.currtxt = srctxtCell.text[0][0];
+      } catch (err) {
+        activeCell.load('text');
+        await ctx.sync();
+        this.currtxt = activeCell.text[0][0];
+      }
+
+      if (!this.currtxt)
+        return;
+
+      this.refs$ = this.http.get<{ r: any[] }>('/api/search', { params: { q: this.currtxt } })
+        .pipe(map(res => res.r));
+    });
   }
 }
